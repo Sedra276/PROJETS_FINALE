d5f1166a8daeea5c0b237f37b7e979f89dc9b645
Lari-it <hanitriniaina0503@gmail.com>
2026-07-20 13:02:47 +0300

    connex

--- ETU004141_ETU004220/app/Config/Routes.php
+++ ETU004141_ETU004220/app/Config/Routes.php
@@ -5,7 +5,7 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'client/connexion');
+$routes->get('/', 'AuthClient::formulaireConnexion');
 
 
 $routes->get('admin/login', 'AuthOperateur::afficherFormulaireConnexion');
